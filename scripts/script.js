ac2081174660df2e8c47e4014c9ea9eb83155b11
wodpachua <molarag21996@gmail.com>
2024-03-07 13:45:33 +0300

    Add Event listeners to square divs to fill them with random color on mouseover

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,22 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
 
 for (let i = 0; i < 100; i++) {
-    const newDiv = document.createElement('div');
-    newDiv.setAttribute('class', 'square');
-    container.appendChild(newDiv)
+  const newDiv = document.createElement("div");
+  newDiv.setAttribute("class", "square");
+  container.appendChild(newDiv);
 }
 
+const squares = document.querySelectorAll(".square");
+squares;
+
+squares.forEach((square) => {
+  square.addEventListener("mouseover", fillColor);
+});
+
+function fillColor(event) {
+  event.target.style.backgroundColor = `rgb(${randomNum()},${randomNum()},${randomNum()})`;
+}
+
+function randomNum() {
+  return Math.floor(Math.random() * 256);
+}
