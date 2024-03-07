217916570fcc7c14f646494f6143bd1dcd087fe1
wodpachua <molarag21996@gmail.com>
2024-03-07 21:13:14 +0300

    create functions to update grid pad with new grid length

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,29 @@ const container = document.querySelector(".container");
 const lengthButton = document.querySelector(".grid-length");
 const clearButton = document.querySelector(".clear");
 
-let gridLength = 10;
+let padActive = false;
 
-for (let i = 0; i < 100; i++) {
-  const newDiv = document.createElement("div");
-  newDiv.setAttribute("class", "square");
-  container.appendChild(newDiv);
-}
-
-const squares = document.querySelectorAll(".square");
-squares;
-
-squares.forEach((square) => {
-  square.addEventListener("mouseover", fillColor);
+// This ensures the pad is activated only after clicking on the pad area.
+container.addEventListener("click", () => {
+  if (!padActive) {
+    activatePad();
+    padActive = true;
+  }
 });
+
+function activatePad(gridLength = 10) {
+  for (let i = 0; i < gridLength * gridLength; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "square");
+    newDiv.setAttribute(
+      "style",
+      `width: calc(600px / ${gridLength}); height: calc(600px / ${gridLength})`
+    );
+    container.appendChild(newDiv);
+
+    newDiv.addEventListener("mouseover", fillColor);
+  }
+}
 
 function fillColor(event) {
   event.target.style.backgroundColor = `rgb(${randomNum()},${randomNum()},${randomNum()})`;
@@ -25,19 +34,22 @@ function randomNum() {
   return Math.floor(Math.random() * 256);
 }
 
-lengthButton.addEventListener('click', upDateGrids);
+// Changing grid length
 
-function getLength() {
-    let length = Number.parseInt(prompt('Enter Number less than "100"'))
-
-    if (length <= 100 && length > 0) {
-        return length;
-    } else {
-        alert(`You entered ${length}. Number should be between "1" and "100"`)
-    }
-}
+lengthButton.addEventListener("click", upDateGrids);
 
 function upDateGrids() {
- gridLength = getLength();
- console.log(gridLength)
+  const gridLength = getLength();
+  container.replaceChildren();
+  activatePad(gridLength);
+}
+
+function getLength() {
+  let length = Number.parseInt(prompt('Enter Number less than "100"'));
+
+  if (length <= 100 && length > 0) {
+    return length;
+  } else {
+    alert(`You entered ${length}. Number should be between "1" and "100"`);
+  }
 }
