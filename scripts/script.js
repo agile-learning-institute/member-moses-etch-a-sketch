a7139a3721c586ebca47d9a45ada2d97c8ea1e28
wodpachua <molarag21996@gmail.com>
2024-03-07 15:30:10 +0300

    Add event listener to button to prompt for grid length

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
 const container = document.querySelector(".container");
+const lengthButton = document.querySelector(".grid-length");
+const clearButton = document.querySelector(".clear");
+
+let gridLength = 10;
 
 for (let i = 0; i < 100; i++) {
   const newDiv = document.createElement("div");
@@ -19,4 +23,21 @@ function fillColor(event) {
 
 function randomNum() {
   return Math.floor(Math.random() * 256);
+}
+
+lengthButton.addEventListener('click', upDateGrids);
+
+function getLength() {
+    let length = Number.parseInt(prompt('Enter Number less than "100"'))
+
+    if (length <= 100 && length > 0) {
+        return length;
+    } else {
+        alert(`You entered ${length}. Number should be between "1" and "100"`)
+    }
+}
+
+function upDateGrids() {
+ gridLength = getLength();
+ console.log(gridLength)
 }
